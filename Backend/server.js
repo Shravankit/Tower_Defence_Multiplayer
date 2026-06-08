a12d9f6d56596848d3481a27c050a06a5675db86
Shravankit <eeswarnaidu111@gmail.com>
2026-06-08 16:57:25 +0530

    corrected players join logic

--- Backend/server.js
+++ Backend/server.js
@@ -5,14 +5,57 @@ const wss = new ws.Server({ port: 8080 });
 console.log("WebSocket Server Running on ws://localhost:8080");
 
 let players = [];
+let playerNames = new Set();
 
 wss.on("connection", (ws) => {
   console.log("Player connected");
+
+  ws.playerName = "";
 
   players.push(ws);
 
   ws.on("message", (msg) => {
     console.log(msg.toString());
+
+    const data = JSON.parse(msg);
+
+    if (data.type === "join") {
+      ws.playerName = data.name;
+
+      console.log(`${ws.playerName} had joined room`);
+
+      playerNames.add(data.name);
+
+      // players.push(ws);
+
+      ws.send(
+        JSON.stringify({
+          type: "joined",
+          name: data.name,
+        }),
+      );
+
+      // players.forEach((player) => {
+      //   if (player !== ws && player.readyState === ws.OPEN)
+      //     player.send(
+      //       JSON.stringify({
+      //         type: "players",
+      //         players: [...playerNames],
+      //       }),
+      //     );
+      // });
+
+      console.log(playerNames);
+
+      players.forEach((player) => {
+        if (player !== ws && player.readyState === ws.OPEN)
+          player.send(
+            JSON.stringify({ type: "playerJoined", name: data.name }),
+          );
+      });
+
+      return;
+    }
 
     players.forEach((player) => {
       if (player !== ws && player.readyState === ws.OPEN) {
@@ -24,5 +67,6 @@ wss.on("connection", (ws) => {
   ws.on("close", () => {
     console.log("closed");
     players = players.filter((player) => player !== ws);
+    playerNames.delete(ws.playerName);
   });
 });
